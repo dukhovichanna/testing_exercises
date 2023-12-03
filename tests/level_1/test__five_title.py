b14ac6f9d1f68d88c8f6e49dcf8e07e7b2f47806
dukhovichanna <dukhovichanna@gmail.com>
2023-12-03 16:23:50 -0500

    Update test__five_title.py

--- tests/level_1/test__five_title.py
+++ tests/level_1/test__five_title.py
@@ -2,14 +2,13 @@ from functions.level_1.five_title import change_copy_item
 import pytest
 
 @pytest.mark.paramterize(
-        "title, max_main_item_title_length, expected_result",
+        'title, max_main_item_title_length, expected_result',
         [
             ('Thermodynamics', None, 'Copy of Thermodynamics'),
             ('Thermodynamics', 5, 'Thermodynamics'),
             ('Copy of Thermodynamics', None, 'Copy of Thermodynamics (2)'),
-            ('Copy of Thermodynamics (2)', None, 'Copy of Thermodynamics (3)')
+            ('Copy of Thermodynamics (2)', None, 'Copy of Thermodynamics (3)'),
             ('Copy of Thermodynamics (2)', 5, 'Copy of Thermodynamics (2)')
-
         ]
 )
 def test__change_copy_item(title, max_main_item_title_length, expected_result):
