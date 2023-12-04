dbf48b265d852281171afa5375fd56af9434e93d
dukhovichanna <dukhovichanna@gmail.com>
2023-12-04 08:07:28 -0500

    add more tests and ids to level1/test3

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,8 +2,10 @@ import pytest
 from functions.level_1.three_url_builder import build_url
 
 @pytest.mark.parametrize("host_name, relative_url, get_params, expected", [
-    ("https://example.com", "path/to/resource", {"param1": "value1", "param2": "value2"}, "https://example.com/path/to/resource?param1=value1&param2=value2"),
-    ("https://example.com", "path/to/resource", None, "https://example.com/path/to/resource"),
+    pytest.param("https://example.com", "path/to/resource", {"param1": "value1", "param2": "value2"}, "https://example.com/path/to/resource?param1=value1&param2=value2", id="with_params"),
+    pytest.param("https://example.com", "path/to/resource", None, "https://example.com/path/to/resource", id="without_params"),
+    pytest.param("https://example.com", "", {"param": "value"}, "https://example.com/?param=value", id="empty_relative_url"),
+    pytest.param("https://example.com", "path/to/resource", {"empty_param": ""}, "https://example.com/path/to/resource?empty_param=", id="empty_param_value")
 ])
 def test_build_url(host_name, relative_url, get_params, expected):
     result = build_url(host_name, relative_url, get_params)
