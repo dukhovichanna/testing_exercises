50cd8b595871ba65f63bfa9739fd0b9d8c0c57af
dukhovichanna <dukhovichanna@gmail.com>
2023-12-04 07:55:00 -0500

    more obvious names

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -4,9 +4,9 @@ from functions.level_1.one_gender import genderalize
 @pytest.mark.parametrize(
         'verb_male, verb_female, gender, expected_result',
         [
-            pytest.param('учился', 'училась', 'male', 'учился', id='male_gender'),
+            pytest.param('учился', 'училась', 'male', 'учился', id='return_true_when_male_gender'),
             pytest.param('учился', 'училась', 'blah', 'учился', id='return_false_for_neither_male_nor_female_gender'),
-            pytest.param('учился', 'училась', 'female', 'училась', id='female_gender')
+            pytest.param('учился', 'училась', 'female', 'училась', id='return_true_when_female_gender')
         ]
 )
 def test_genderalize(verb_male, verb_female, gender, expected_result):
