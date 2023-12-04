e6d9412bb8c09ac94c7a8d1fd9d95adb4a4ebc3d
dukhovichanna <dukhovichanna@gmail.com>
2023-12-04 07:28:56 -0500

    parametrize level_1\test_one_gender.py

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,8 +1,28 @@
+import pytest
 from functions.level_1.one_gender import genderalize
 
+@pytest.mark.parametrize(
+        'verb_male, verb_female, gender, expected_result',
+        [
+            ('учился', 'училась', 'male', 'учился'),
+            ('учился', 'училась', 'blah', 'учился'),
+            ('учился', 'училась', 'female', 'училась')
+        ]
+)
+def test_genderalize(verb_male, verb_female, gender, expected_result):
+    result = genderalize(verb_male, verb_female, gender)
+    
+    if gender not in ('male','female'):
+        assert result is not expected_result
+    else:
+        assert result is expected_result
 
-def test_genderalize():
-    assert genderalize('учился','училась','male') == 'учился'
-    assert not genderalize('учился','училась','blah') == 'учился'
-    assert genderalize('учился','училась','female') == 'училась'
+@pytest.mark.parametrize(
+        'verb_male, verb_female, gender, expected_result',
+        [
 
+            ('учился', 'училась', 'blah', 'учился')
+        ]
+)
+def test_genderalize_false(verb_male, verb_female, gender, expected_result):
+    assert genderalize(verb_male, verb_female, gender) is not expected_result
