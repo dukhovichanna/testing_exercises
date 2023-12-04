631d976c9e0792f99f5c88a6df8d8ec4c0662d42
dukhovichanna <dukhovichanna@gmail.com>
2023-12-04 11:00:47 -0500

    add xfail to test level1/test1

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -5,14 +5,11 @@ from functions.level_1.one_gender import genderalize
         'verb_male, verb_female, gender, expected_result',
         [
             pytest.param('учился', 'училась', 'male', 'учился', id='return_true_when_male_gender'),
-            pytest.param('учился', 'училась', 'blah', 'учился', id='return_false_for_neither_male_nor_female_gender'),
+            pytest.param('учился', 'училась', 'blah', 'учился', id='return_false_for_neither_male_nor_female_gender', marks=pytest.mark.xfail(reason='gender is not valid')),
             pytest.param('учился', 'училась', 'female', 'училась', id='return_true_when_female_gender')
         ]
 )
 def test_genderalize(verb_male, verb_female, gender, expected_result):
     result = genderalize(verb_male, verb_female, gender)
-    
-    if gender not in ('male','female'):
-        assert result is not expected_result
-    else:
-        assert result is expected_result
+
+    assert result is expected_result
