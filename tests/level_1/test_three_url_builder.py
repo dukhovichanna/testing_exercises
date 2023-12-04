cd37660624501b40c3447a6cb050778be7879a99
dukhovichanna <dukhovichanna@gmail.com>
2023-12-04 07:34:25 -0500

    parametrize level_1\test_3

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -1,12 +1,10 @@
+import pytest
 from functions.level_1.three_url_builder import build_url
 
-
-def test_build_url():
-    host_name = "https://example.com"
-    relative_url = "path/to/resource"
-    get_params = {"param1": "value1", "param2": "value2"}
-    assert build_url(host_name, relative_url, get_params) == "https://example.com/path/to/resource?param1=value1&param2=value2"
-
-    host_name = "https://example.com"
-    relative_url = "path/to/resource"
-    assert build_url(host_name, relative_url) == "https://example.com/path/to/resource"
+@pytest.mark.parametrize("host_name, relative_url, get_params, expected", [
+    ("https://example.com", "path/to/resource", {"param1": "value1", "param2": "value2"}, "https://example.com/path/to/resource?param1=value1&param2=value2"),
+    ("https://example.com", "path/to/resource", None, "https://example.com/path/to/resource"),
+])
+def test_build_url(host_name, relative_url, get_params, expected):
+    result = build_url(host_name, relative_url, get_params)
+    assert result == expected
