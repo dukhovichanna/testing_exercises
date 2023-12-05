e659cc66fda43455e33b499d5f2fd6d34f642664
dukhovichanna <dukhovichanna@gmail.com>
2023-12-05 15:05:41 -0500

    parametrize level2/test3

--- tests/level_2/test_three_first.py
+++ tests/level_2/test_three_first.py
@@ -1,19 +1,21 @@
-from functions.level_2.three_first import first, NOT_SET
 import pytest
+from functions.level_2.three_first import first, NOT_SET
 
-def test__first__with_items():
-    assert first([1,2,3]) == 1
-
-def test__first__with_empty_items_and_int_default():
-    assert first([],3) == 3
-
-def test__first__with_empty_items_and_str_default():
-    assert first([],'test') == 'test'
-
-def test__first__with_empty_items_and_no_default():
-    with pytest.raises(AttributeError):
-        first([])
-
-def test__first__with_empty_items_and_default_set_to_not_set():
-    with pytest.raises(AttributeError):
-        first([], default=NOT_SET)
+@pytest.mark.parametrize(
+    "items, default, expected",
+    [
+        pytest.param([1, 2, 3], NOT_SET, 1, id='return_first_item_with_non_empty_items'),
+        pytest.param([], 3, 3, id='return_default_with_empty_items_and_int_default'),
+        pytest.param([], 'test', 'test', id='return_default_with_empty_items_and_str_default'),
+        pytest.param([], NOT_SET, None, id='raise_error_with_empty_items_and_no_default'),
+        pytest.param([], NOT_SET, None, id='raise_error_with_empty_items_and_default_set_to_not_set'),
+    ]
+)
+def test__first(items, default, expected):
+    if (default == NOT_SET) and (items == []):
+        with pytest.raises(AttributeError):
+            result = first(items)
+    else:
+        result = first(items, default)
+        
+        assert result == expected
