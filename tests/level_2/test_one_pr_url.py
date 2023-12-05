99ce6b9e4010e4b88c78c96d1024c476bf1be6f6
dukhovichanna <dukhovichanna@gmail.com>
2023-12-05 14:29:56 -0500

    add more tests to level2/test1

--- tests/level_2/test_one_pr_url.py
+++ tests/level_2/test_one_pr_url.py
@@ -6,7 +6,9 @@ from functions.level_2.one_pr_url import is_github_pull_request_url
         [
             pytest.param('https://github.com/dukhovichanna/testing_exercises/pull/1',True, id='return_true_for_pull_request_page_conversation_page'),
             pytest.param('https://github.com/dukhovichanna/testing_exercises/pull/1/files', False, id='return_false_forpull_request_page_files_page'),
-            pytest.param('https://github.com/dukhovichanna/testing_exercises', False, id='return_false_on_non_pr_url')
+            pytest.param('https://github.com/dukhovichanna/testing_exercises', False, id='return_false_on_non_pr_url'),
+            pytest.param('https://example.com/dukhovichanna/testing_exercises/pull/1', False, id='return_false_for_non_github_url'),
+            pytest.param('https://github.com/dukhovichanna/testing_exercises/pull/1/commits', False, id='return_false_for_pull_request_commits_page')
         ]
 )
 
