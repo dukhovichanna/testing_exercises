dac697233f94c3ec6ac8545ca6500d33e4b10e67
dukhovichanna <dukhovichanna@gmail.com>
2023-12-05 17:36:52 -0500

    parametrize level2/test4

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -1,56 +1,27 @@
+import pytest
 from functions.level_2.four_sentiment import check_tweet_sentiment
 
-def test__check_tweet_sentiment__neutral_sentiment():
-    text = "This is a neutral tweet."
-    good_words = {"happy", "positive"}
-    bad_words = {"sad", "negative"}
-
+@pytest.mark.parametrize(
+    "text, good_words, bad_words, expected",
+    [
+        pytest.param("This is a neutral tweet.", 
+                     {"happy", "positive"}, {"sad", "negative"}, 
+                     None, id='neutral_sentiment'),
+        pytest.param("I love this product, it's amazing!", 
+                     {"love", "amazing", "positive"}, {"hate", "terrible", "negative"}, 
+                     "GOOD", id='positive_sentiment'),
+        pytest.param("I hate waiting in long lines, it's terrible!", 
+                     {"love", "amazing", "positive"}, {"hate", "terrible", "negative"}, 
+                     "BAD", id='negative_sentiment'),
+        pytest.param("This is a mixed review, both positive and negative aspects.", 
+                     {"love", "amazing", "positive"}, {"hate", "terrible", "negative"}, 
+                     None, id='equal_positive_negative_words'),
+        pytest.param("I LOVE this product, it's AMAZING!", {"love", "amazing", "positive"}, {"hate", "terrible", "negative"}, 
+                     "GOOD", id='case_insensitive_matching'),
+        pytest.param("", {"happy", "positive"}, {"sad", "negative"}, 
+                     None, id='empty_input'),
+    ]
+)
+def test__check_tweet_sentiment(text, good_words, bad_words, expected):
     result = check_tweet_sentiment(text, good_words, bad_words)
-
-    assert result is None
-
-def test__check_tweet_sentiment__positive_sentiment():
-    text = "I love this product, it's amazing!"
-    good_words = {"love", "amazing", "positive"}
-    bad_words = {"hate", "terrible", "negative"}
-
-    result = check_tweet_sentiment(text, good_words, bad_words)
-
-    assert result == "GOOD"
-
-def test__check_tweet_sentiment__negative_sentiment():
-    text = "I hate waiting in long lines, it's terrible!"
-    good_words = {"love", "amazing", "positive"}
-    bad_words = {"hate", "terrible", "negative"}
-
-    result = check_tweet_sentiment(text, good_words, bad_words)
-    
-    assert result == "BAD"
-
-def test__check_tweet_sentiment__equal_positive_negative_words():
-    text = "This is a mixed review, both positive and negative aspects."
-    good_words = {"love", "amazing", "positive"}
-    bad_words = {"hate", "terrible", "negative"}
-
-    result = check_tweet_sentiment(text, good_words, bad_words)
-
-    assert result is None
-
-def test__check_tweet_sentiment__case_insensitive_matching():
-    text = "I LOVE this product, it's AMAZING!"
-    good_words = {"love", "amazing", "positive"}
-    bad_words = {"hate", "terrible", "negative"}
-
-    result = check_tweet_sentiment(text, good_words, bad_words)
-
-    assert result == "GOOD"
-
-def test__check_tweet_sentiment__empty_input():
-    text = ""
-    good_words = {"happy", "positive"}
-    bad_words = {"sad", "negative"}
-
-    result = check_tweet_sentiment(text, good_words, bad_words)
-    
-    assert result is None
-
+    assert result == expected
