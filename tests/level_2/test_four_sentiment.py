7f7fc5933d39ed914c2deef43412bcf7958d13c1
dukhovichanna <dukhovichanna@gmail.com>
2023-12-05 17:38:16 -0500

    update tests names

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -6,7 +6,7 @@ from functions.level_2.four_sentiment import check_tweet_sentiment
     [
         pytest.param("This is a neutral tweet.", 
                      {"happy", "positive"}, {"sad", "negative"}, 
-                     None, id='neutral_sentiment'),
+                     None, id='return_none_when_neutral_sentiment'),
         pytest.param("I love this product, it's amazing!", 
                      {"love", "amazing", "positive"}, {"hate", "terrible", "negative"}, 
                      "GOOD", id='positive_sentiment'),
@@ -15,11 +15,11 @@ from functions.level_2.four_sentiment import check_tweet_sentiment
                      "BAD", id='negative_sentiment'),
         pytest.param("This is a mixed review, both positive and negative aspects.", 
                      {"love", "amazing", "positive"}, {"hate", "terrible", "negative"}, 
-                     None, id='equal_positive_negative_words'),
+                     None, id='return_none_when_equal_positive_negative_words'),
         pytest.param("I LOVE this product, it's AMAZING!", {"love", "amazing", "positive"}, {"hate", "terrible", "negative"}, 
                      "GOOD", id='case_insensitive_matching'),
         pytest.param("", {"happy", "positive"}, {"sad", "negative"}, 
-                     None, id='empty_input'),
+                     None, id='return_none_when_empty_input'),
     ]
 )
 def test__check_tweet_sentiment(text, good_words, bad_words, expected):
