e2428cdae519e129f1f7d8089c407c98891b4591
dukhovichanna <dukhovichanna@gmail.com>
2023-12-16 21:34:05 -0500

    add more expenses

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,36 @@ def sample_bank_card():
     return BankCard(last_digits='1234', owner='Jane')
 
 @pytest.fixture
-def sample_expenses(sample_bank_card):
+def spent_at():
+    return datetime.datetime(2023, 11, 23, 15, 29)
+
+@pytest.fixture
+def sample_expense_with_trigger(spent_at):
+    return Expense(
+        amount=decimal.Decimal('1000.00'),
+        currency='USD',
+        card=None,
+        spent_in='Moscow Cinema',
+        spent_at=spent_at,
+        category=None
+    )
+
+@pytest.fixture
+def sample_expense_without_trigger(spent_at):
+    return Expense(
+        amount=decimal.Decimal('250.00'),
+        currency='USD',
+        card=None,
+        spent_in='Random Store',
+        spent_at=spent_at,
+        category=None
+    )
+
+@pytest.fixture
+def sample_expenses(sample_bank_card, sample_expense_with_trigger, sample_expense_without_trigger):
     expenses = [
+        sample_expense_with_trigger,
+        sample_expense_with_trigger,
         Expense(
         amount=decimal.Decimal('1000.00'),
         currency=Currency.USD,
--- tests/level_3/test_two_expense_categorizer.py
+++ tests/level_3/test_two_expense_categorizer.py
@@ -1,3 +1,27 @@
 import pytest
 from functions.level_3.two_expense_categorizer import guess_expense_category, is_string_contains_trigger
+from functions.level_3.models import ExpenseCategory
 
+def test__guess_expense_category__successful_detection(sample_expense_with_trigger):
+    result = guess_expense_category(sample_expense_with_trigger)
+    assert result == ExpenseCategory.THEATRES_MOVIES_CULTURE
+
+def test__guess_expense_category__no_trigger_detected(sample_expense_without_trigger):
+    result = guess_expense_category(sample_expense_without_trigger)
+    assert result is None
+
+def test__is_string_contains_trigger__starts_with(sample_expense_with_trigger):
+    result = is_string_contains_trigger(sample_expense_with_trigger.spent_in, "moscow")
+    assert result is True
+
+def test__is_string_contains_trigger__ends_with(sample_expense_with_trigger):
+    result = is_string_contains_trigger(sample_expense_with_trigger.spent_in, "cinema")
+    assert result is True
+
+def test__is_string_contains_trigger__in_middle(sample_expense_with_trigger):
+    result = is_string_contains_trigger(sample_expense_with_trigger.spent_in, "cinema")
+    assert result is True
+
+def test__is_string_contains_trigger__not_present(sample_expense_without_trigger):
+    result = is_string_contains_trigger(sample_expense_without_trigger.spent_in, "cinema")
+    assert result is False
