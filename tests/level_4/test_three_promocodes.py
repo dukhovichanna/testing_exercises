29b282ef2cb6197756e297ee557e1b1f1b3bf781
dukhovichanna <dukhovichanna@gmail.com>
2023-12-23 23:00:15 -0500

    Create test_three_promocodes.py

---
+++ tests/level_4/test_three_promocodes.py
@@ -0,0 +1,28 @@
+import string
+import pytest
+from functions.level_4.three_promocodes import generate_promocode
+
+def test__generate_promocode__return_true_if_default_length_is_8():
+    code = generate_promocode()
+    assert len(code) == 8
+
+def test__generate_promocode__return_true_if_code_contains_only_alphabetic_characters():
+    code = generate_promocode()
+    assert code.isalpha()
+
+def test__generate_promocode__return_true_if_code_len_equals_custom_length():
+    code = generate_promocode(12)
+    assert len(code) == 12
+
+def test__generate_promocode__return_empty_str_for_invalid_length():
+    code = generate_promocode(-1)
+    assert len(code) == 0
+
+def test_generate_promocode_return_empty_str_for_zero_length():
+    code = generate_promocode(0)
+    assert len(code) == 0
+
+def test__generate_promocode__return_true_if_promocode_is_uppercase():
+    code = generate_promocode(10)
+    assert code.isupper()
+
